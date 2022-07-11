1e9a372243dbe07d62d061e28f31d054ae15bba4
Dominic <dominic.alie@gmail.com>
2022-07-11 19:46:37 -0400

    Responsive style

--- apps/carto-wizard/src/app/play/ask-country/ask-country.tsx
+++ apps/carto-wizard/src/app/play/ask-country/ask-country.tsx
@@ -12,6 +12,10 @@ export interface AskCountryProps {
 export function AskCountry(props: AskCountryProps) {
   const [items, setItems] = useState<Country[] | null>();
 
+  const isLongString = (value: string) => {
+    return value.length > 18;
+  };
+
   useEffect(() => {
     setItems([props.country, ...props.guesses].sort(() => 0.5 - Math.random()));
   }, [props.country, props.guesses]);
@@ -23,8 +27,10 @@ export function AskCountry(props: AskCountryProps) {
       exit={{ transform: 'translateY(6rem)' }}
     >
       {items?.map((i) => (
-        <CountryDiv>
-          <FlagImg alt={i.name} src={i.flags} />
+        <CountryDiv isLongString={isLongString(i.name)}>
+          <FlagContainer>
+            <FlagImg alt={i.name} src={i.flags} />
+          </FlagContainer>
           {i.name}
         </CountryDiv>
       ))}
@@ -40,30 +46,42 @@ const Container = styled(motion.div)`
   left: 0;
   right: 0;
   z-index: 1000;
-
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  justify-content: center;
-  padding: 0 1rem;
+  justify-content: space-between;
+  padding: 1rem;
   gap: 1rem;
   //height: 5rem;
   background-color: var(--dark);
   box-shadow: 0 -0.25rem 1rem #000;
 `;
 
-const CountryDiv = styled.div`
+const CountryDiv = styled.div<{ isLongString: boolean }>`
   display: flex;
   gap: 1rem;
   align-items: center;
-  justify-content: center;
-  font-size: 1.5rem;
-  max-width: 50%;
-  max-height: 50%;
+  justify-content: start;
+  font-size: ${(props) => (props.isLongString ? '1rem' : '1.5rem')};
+
+  @media (max-width: 700px) {
+    width: 90vw;
+  }
+
+  @media (min-width: 700px) and (max-width: 1200px) {
+    width: 40vw;
+  }
+`;
+
+const FlagContainer = styled.div`
+  aspect-ratio: 16/9;
+  width: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: right;
 `;
 
 const FlagImg = styled.img`
   box-shadow: 0 0 0.75rem;
-  max-width: 6rem;
-  max-height: 2rem;
+  max-height: 100%;
 `;
